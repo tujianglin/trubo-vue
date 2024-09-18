239460c8d4aa45eef94ff253aa20623f5ff25320
涂江林 <jianglin.tu@wimisoft.com>
2024-09-18 13:48:57 +0800

    feat: 1

--- apps/app1/src/App.vue
+++ apps/app1/src/App.vue
@@ -1,32 +1,8 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="tsx">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  setup() {
+    return () => <div>111</div>;
+  },
+});
 </script>
-
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  transition: filter 300ms;
-  will-change: filter;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
