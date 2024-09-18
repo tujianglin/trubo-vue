07e03896cc441fdc6a5dedfb91b033dea98d6eed
涂江林 <jianglin.tu@wimisoft.com>
2024-09-18 13:43:13 +0800

    feat: 1

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,20 +1,20 @@
 export default {
   '*.{js,jsx,ts,tsx}': [
-    'prettier --cache --ignore-unknown  --write',
-    'eslint --cache --fix',
+    'prettier --ignore-unknown  --write',
+    'eslint --fix',
   ],
   '*.{scss,less,styl,html,vue,css}': [
-    'prettier --cache --ignore-unknown --write',
+    'prettier --ignore-unknown --write',
     'stylelint --fix --allow-empty-input',
   ],
-  '*.md': ['prettier --cache --ignore-unknown --write'],
+  '*.md': ['prettier --ignore-unknown --write'],
   '*.vue': [
     'prettier --write',
-    'eslint --cache --fix',
+    'eslint --fix',
     'stylelint --fix --allow-empty-input',
   ],
   '{!(package)*.json,*.code-snippets,.!(browserslist)*rc}': [
-    'prettier --cache --write--parser json',
+    'prettier --write--parser json',
   ],
-  'package.json': ['prettier --cache --write'],
+  'package.json': ['prettier --write'],
 };
