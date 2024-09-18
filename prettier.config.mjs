8e8186e3b0c01dbf56921c2bd3d0c4b7d8ed04fc
涂江林 <jianglin.tu@wimisoft.com>
2024-09-18 15:06:47 +0800

    feat: 1

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,1 @@
-/** @type {import('prettier').Config} */
-export default {
-  endOfLine: 'auto',
-  plugins: ['prettier-plugin-tailwindcss'],
-  printWidth: 80,
-  proseWrap: 'never',
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+export { default } from '@wm/prettier-config'
