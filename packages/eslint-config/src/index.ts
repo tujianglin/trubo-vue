4f50e5974de1104a1df789b7312d1c5882aa38e1
涂江林 <jianglin.tu@wimisoft.com>
2024-09-18 13:50:03 +0800

    feat: 1

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -3,4 +3,22 @@ import pluginVue from 'eslint-plugin-vue';
 import tseslint from 'typescript-eslint';
 import { ignores } from './configs';
 
-export default tseslint.config(...ignores(), {});
+export default tseslint.config(...ignores(), {
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    ...pluginVue.configs['flat/recommended'],
+  ],
+  files: ['*.vue', '**/*.vue'],
+  languageOptions: {
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      parser: '@typescript-eslint/parser',
+    },
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  },
+});
