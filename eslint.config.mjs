f0a9e2c636a705f60e39430a08b30eae87c5ad8b
涂江林 <jianglin.tu@wimisoft.com>
2024-09-18 13:52:00 +0800

    feat: 1

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
-import defineConfig from '@wm/eslint-config'
+import js from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue';
+import unocss from '@unocss/eslint-config/flat';
+import tseslint from 'typescript-eslint';
 
-export default defineConfig
+export default tseslint.config(
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, ...pluginVue.configs['flat/recommended']],
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        parser: '@typescript-eslint/parser',
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+)
